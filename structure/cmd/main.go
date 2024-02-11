6a94304ff93269fe95c8892bbb71f83543aebd6b
Youssef Mohamed <uif721@gmail.com>
2024-02-11 23:46:30 +0200

    Fix: Restructure

--- structure/cmd/main.go
+++ structure/cmd/main.go
@@ -19,25 +19,6 @@ func main(){
 		port ="8000"
 	}
 	
-	// // connect to database
-	// 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-	// 	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	// 	db := options.Client().ApplyURI("mongodb+srv://Youssef:<password>@cluster0.wotxocw.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
-	// 	// Create a new client and connect to the server
-	// 	client, err := mongo.Connect(context.TODO(), db)
-	// 	if err != nil {
-	// 	  panic(err)
-	// 	}
-	// 	defer func() {
-	// 	  if err = client.Disconnect(context.TODO()); err != nil {
-	// 		panic(err)
-	// 	  }
-	// 	}()
-	// 	// Send a ping to confirm a successful connection
-	// 	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	// 	  panic(err)
-	// 	}
-	// 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
 	//create router
 	router := gin.New()
 	router.Use(gin.Logger())
