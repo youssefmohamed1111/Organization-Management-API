5d0fa28484f4092cf1830ab3e9138991f3772bbe
Youssef Mohamed <uif721@gmail.com>
2024-02-12 00:52:15 +0200

    Feat: Added Database (MongoDB)

---
+++ controllers/userController.go
@@ -0,0 +1,15 @@
+package controllers
+import(
+
+)
+func HashPassword()
+
+func VerifyPassword()
+
+func SignUp()
+
+func Signin()
+
+func GetUsers()
+
+func GetUser()
--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,12 +1,17 @@
 package database
-import(
-"fmt"
-"log"
-"os"
-"context"
-"github.com/joho/godotenv"
-"go.mongo.org/mongo-driver/mongo"
-"go.mongo.org/mongo-driver/mongo/options"
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongo.org/mongo-driver/mongo"
+	"go.mongo.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 func DBinstance() *mongo.Client{
 //connect to database
@@ -14,23 +19,25 @@ func DBinstance() *mongo.Client{
 	if err != nil {
 		log.Fatal("ERROR Loading .env File")
 	}
-	MongoDb= os.Getenv("MONGODB_URL")
-		// // Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-		// serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-		// db := options.Client().ApplyURI("mongodb+srv://Youssef:<password>@cluster0.wotxocw.mongodb.net/?retryWrites=true&w=majority").SetServerAPIOptions(serverAPI)
-		// // Create a new client and connect to the server
-		// client, err := mongo.Connect(context.TODO(), db)
-		// if err != nil {
-		//   panic(err)
-		// }
-		// defer func() {
-		//   if err = client.Disconnect(context.TODO()); err != nil {
-		// 	panic(err)
-		//   }
-		// }()
-		// // Send a ping to confirm a successful connection
-		// if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-		//   panic(err)
-		// }
-		// fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+	// Getting Connection String
+	MongoDb := os.Getenv("MONGODB_URL")
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDb))
+	if err != nil {
+		  log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(),10 *time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+ 	 }		
+		fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+		return client
+}
+ // Initializing a DB instance
+var Client *mongo.Client = DBinstance()
+func OpenCollection(client *mongo.Client, CollectionName string) *mongo.Collection{
+	var collection *mongo.collection= client.Database("cluster0").Collection(CollectionName)
+	return collection
+
 }
